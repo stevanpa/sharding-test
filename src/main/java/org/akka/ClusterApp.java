b10d49558329fac6ba2df03e7f91ca8c6b65f960
Stevan Pavlicic <stevan.pavlicic@gmail.com>
2018-01-27 12:27:14 +0100

    restart

---
+++ src/main/java/org/akka/ClusterApp.java
@@ -0,0 +1,35 @@
+package org.akka;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
+// mvn exec:java -Dexec.mainClass="org.akka.ClusterApp" -Dexec.args="2551"
+public class ClusterApp {
+
+	public static void main(String[] args) {
+		if (args.length == 0)
+			startup(new String[] { "2551", "2552", "0" });
+		else
+			startup(args);
+	}
+
+	public static void startup(String[] ports) {
+		for (String port : ports) {
+			// Override the configuration of the port
+			Config config = ConfigFactory.parseString(
+				"akka.remote.netty.tcp.port=" + port + "\n" +
+				"akka.remote.artery.canonical.port=" + port)
+			.withFallback(ConfigFactory.load());
+
+		// Create an Akka system
+		ActorSystem system = ActorSystem.create("ClusterSystem", config);
+
+		// Create an actor that handles cluster domain events
+		system.actorOf(Props.create(ClusterListener.class),
+				"clusterListener");
+		}
+	}
+}
